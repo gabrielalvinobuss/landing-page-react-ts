52a295fac90f9929a76f0fc0f5cca032d2b3b94f
Gabriel Buss <gabrielalvinobuss@gmail.com>
2024-10-06 00:35:59 -0700

    finished creating helping section, updated FlexGrid component and updated index styles to accomplish the helping section correctly

--- src/components/FlexGrid/FlexGrid.tsx
+++ src/components/FlexGrid/FlexGrid.tsx
@@ -4,19 +4,32 @@ type FlexGridParams = {
   children: React.ReactNode;
   center1440: boolean;
   padding104?: boolean;
+  gap72?: boolean;
 };
 
-function getClassName(center1440: boolean, padding104?: boolean) {
+function getClassName(
+  center1440: boolean,
+  padding104?: boolean,
+  gap72?: boolean
+) {
   const is104: string = padding104 === true ? " padding-104-lr" : "";
 
   const is1440: string = center1440 === true ? " center-1440" : "";
 
-  return "FlexGrid flex-grid" + is1440 + is104;
+  const isGap72: string = gap72 === true ? " gap-72" : "";
+
+  return "FlexGrid flex-grid" + is1440 + is104 + isGap72;
 }
 
 function FlexGrid(params: FlexGridParams) {
   return (
-    <div className={getClassName(params.center1440, params.padding104)}>
+    <div
+      className={getClassName(
+        params.center1440,
+        params.padding104,
+        params.gap72
+      )}
+    >
       {params.children}
     </div>
   );
