de0a48408115f7e66160f6ba8683f0b165e70f68
Gabriel Buss <gabrielalvinobuss@gmail.com>
2024-10-13 02:55:59 -0700

    updating div text from the CommunityModule component, unsucessful.

--- src/components/CommunityModule/CommunityModule.tsx
+++ src/components/CommunityModule/CommunityModule.tsx
@@ -1,5 +1,6 @@
 import "./CommunityModule.css";
 import CommunityModuleImage1 from "../../../src/assets/community-updates1.png";
+import FlexGrid from "../FlexGrid/FlexGrid";
 // import CommunityModuleImage2 from "../../../src/assets/community-updates2.png";
 // import CommunityModuleImage3 from "../../../src/assets/community-updates3.png";
 
@@ -11,14 +12,19 @@ type CommunityModuleParams = {
 function CommunityModule(params: CommunityModuleParams) {
   if (params.module === "1") {
     return (
-      <div>
-        <div className="image-box-community-updates">
-          <img src={CommunityModuleImage1} />
+      <FlexGrid center1440>
+        <div className="community-updates-grid justify-content-center padding-top-16">
+          <div className="image-box-community-updates">
+            <img src={CommunityModuleImage1} />
+          </div>
+          {/* NAO ESTA PEGANDO AS FORMATACOES DO CSS NO TEXTO, EU TESTAVA TENTANDO SEPARAR POR DIV PARA VER SE É ISSO, POIS PRECISO DE TODAS ESSAS ABAIXO NA MESMA DIV EM CIMA DA IMAGEM. */}
+          <div className="background-grey justify-content-center box-shadow-community-updates-content-box ">
+            <div className="headline-4 medium-grey text-centralized community-updates-content-box">
+              {params.text}
+            </div>
+          </div>
         </div>
-        <div className="background-grey community-updates-content-box justify-content-center box-shadow-community-updates-content-box">
-          <h2 className="medium-grey text-centralized">{params.text}</h2>
-        </div>
-      </div>
+      </FlexGrid>
     );
   }
 }
