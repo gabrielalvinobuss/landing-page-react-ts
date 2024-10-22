5aa9cf19709f6e9425ae822d711d19b90577622b
Giordano Lazzareschi <giolazza@MacBook-Air-de-Giordano.local>
2024-10-22 20:07:10 -0300

    Added server data for menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,32 +2,72 @@ import "./Header.css";
 import logo from "../../assets/logo.svg";
 import FlexGrid from "../FlexGrid/FlexGrid";
 import BasicButton from "../BasicButton/BasicButton";
+import { useEffect, useState } from "react";
+
+const menuItems: string[] = [
+  "Home",
+  "Service",
+  "Feature",
+  "Product",
+  "Testimonial",
+  "FAQ",
+];
+
+type TemplateDataType = {
+  logo: string;
+  menuItems: string[];
+  loginLabel: string;
+  signUpLabel: string;
+};
+
+function getServerData(): TemplateDataType {
+  return {
+    loginLabel: "Login",
+    logo: logo,
+    menuItems: menuItems,
+    signUpLabel: "Sign Up",
+  };
+}
 
 function Header() {
+  const [templateData, setTemplateData] = useState<TemplateDataType>({
+    logo: "",
+    menuItems: [],
+    loginLabel: "",
+    signUpLabel: "",
+  });
+
+  useEffect(() => {
+    const tempData = getServerData();
+
+    setTemplateData(tempData);
+  }, []);
+
   return (
     <div className="Header">
       <div className="background-grey">
         <FlexGrid center1440 padding104>
           <div>
-            <img className="logo-header" src={logo} />
+            <img className="logo-header" src={templateData.logo} />
           </div>
           <div className="menu-button-bar">
-            <div className="cursor-pointer">Home</div>
-            <div className="cursor-pointer">Service</div>
-            <div className="cursor-pointer">Feature</div>
-            <div className="cursor-pointer">Product</div>
-            <div className="cursor-pointer">Testimonial</div>
-            <div className="cursor-pointer">FAQ</div>
+            {templateData.menuItems.map((menuItem, index) => {
+              return (
+                <div key={index} className="cursor-pointer">
+                  {menuItem}
+                </div>
+              );
+            })}
           </div>
           <div className="login-signup">
             <BasicButton
-              label={"Login"}
+              label={templateData.loginLabel}
               size={"medium"}
               color={"transparent"}
               settings={"login"}
             ></BasicButton>
             <BasicButton
-              label={"Sign up"}
+              label={templateData.signUpLabel}
               size={"medium"}
               color={"green"}
               settings={"login"}
