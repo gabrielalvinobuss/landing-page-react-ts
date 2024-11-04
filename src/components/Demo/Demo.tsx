7ab674db93cd3d1babb70ca9a6ee945533ffebd9
Gabriel Buss <gabrielalvinobuss@gmail.com>
2024-11-04 04:08:28 -0800

    finished new logic

--- src/components/Demo/Demo.tsx
+++ src/components/Demo/Demo.tsx
@@ -1,14 +1,38 @@
+import { useEffect, useState } from "react";
 import BasicButton from "../BasicButton/BasicButton";
 import "./Demo.css";
 
+type DemoParams = {
+  title: string;
+  buttonLabel: string;
+};
+
+function getServerData(): DemoParams {
+  return {
+    title: "Pellentesque suscipit fringilla libero eu.",
+    buttonLabel: "Get a Demo",
+  };
+}
+
 function Demo() {
+  const [demoData, setDemoData] = useState<DemoParams>({
+    title: "",
+    buttonLabel: "",
+  });
+
+  useEffect(() => {
+    const tempData = getServerData();
+
+    setDemoData(tempData);
+  }, []);
+
   return (
     <div className="Demo background-grey">
       <div className="headline-1 neutral-black-text text-centralized content-area padding-top-32">
-        Pellentesque suscipit fringilla libero eu.
+        {demoData.title}
       </div>
       <BasicButton
-        label={"Get a Demo"}
+        label={demoData.buttonLabel}
         size={"normal"}
         color={"green"}
         settings={"centralized"}
